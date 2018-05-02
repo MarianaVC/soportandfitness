4e5fdbb9f2ca3ab6c9f6aa4d63410e91b5b61f03
geny <geny@kamikazelab.com>
2018-05-02 00:36:04 -0500

    funcionalidad video en carrusel

--- public/js/home.js
+++ public/js/home.js
@@ -7,7 +7,7 @@ var Home = function(){
 			singleItem : true,
 			items: 1,
 			nav: true,
-			loop: true,
+			loop: false,
 			navText: ["<",">"]
 		});
 	}
@@ -25,9 +25,49 @@ var Home = function(){
 			$('.smallCarousel').owlCarousel(options);
 	}
 
+	function playVideo(){
+		if( $("body").hasClass("desktop") ) {
+			$(".full-screen-video").each(function(){
+				var videoCont = $(this);
+				var attributes = {
+					'id': 'video-player',
+					'class': 'video-js vjs-default-skin',
+					'width': 1920,
+					'height': 1080,
+					'poster': videoCont.data('poster'),
+					'controls': false,
+					'preload': 'auto',
+					'autoplay': false,
+					'loop': true,
+					'muted': true,
+					'data-setup': '{ "controls": false, "preload": "auto" }'
+				};
+
+				var source = document.createElement('source');
+				$(source).attr('type', 'video/mp4');
+				$(source).attr('src', videoCont.data('video'));
+				var obj = $('<video />').attr(attributes);
+				videoCont.append(obj);
+				$(obj).append(source);
+
+
+				videoP = document.getElementById('video-player');
+				videoP.addEventListener('loadeddata', function() {
+					videoCont.addClass("loaded");
+					//videoPlayer = videojs('video-player');
+					//videoPlayer.play();// Video is loaded and can be played
+					videoP.play();
+				}, false);
+			});
+		}
+		else{
+			$(".full-screen-video").remove();
+		}
+	}
+
 	function start(){
+		playVideo();
 		mainSlider();
-		smallCarousel();
 	}
 	
 	return{
