ae746b3d7af650013eb7e4aa8e9b413b89ea9b43
Mariana Valdivia <mariana@kamikazelab.com>
2018-06-12 15:31:42 -0500

    routes

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,7 @@ exports = module.exports = function (app) {
 	app.get('/nosotros', routes.views.about);
 	app.get('/contacto', routes.views.contact);
 	app.get('/convenios', routes.views.partners);
+	app.get('/instalaciones', routes.views.facilities);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
