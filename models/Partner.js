9d606cf3a96837f7aaa0695d045b91bd3422be1c
Mariana Valdivia <mariana@kamikazelab.com>
2018-06-13 11:01:56 -0500

    imagenes y pdf para promos

--- models/Partner.js
+++ models/Partner.js
@@ -14,11 +14,35 @@ var Partner = new keystone.List('Partner',{
 });
 
 Partner.add('Partner',{
-  desktop_image: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen para escritorio' },
-  mobile_image: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen para teléfono' },
+  image: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen para escritorio' },
+  image_mobile: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen para teléfono' },
   image_alt: {type: String, initial:true , required: true,label:'Alt de imagen' },
 
 });
+
+
+/* acomplar url de cloudinary para traer imagen ligera */ 
+Slide.schema.virtual('image_mobile.cloudinaryURL').get(function () {
+  if ( this.image_mobile ) {
+    image = this.image_mobile;
+    return 'https://res.cloudinary.com/deytb0jub/image/upload/f_auto/q_auto/fl_lossy/v' + image.version + '/' + image.public_id + '.' + image.format;
+  }
+  else{
+    return false;
+  }
+});
+
+Slide.schema.virtual('image.cloudinaryURL').get(function () {
+  if ( this.image ) {
+    image = this.image;
+    return 'https://res.cloudinary.com/deytb0jub/image/upload/f_auto/q_auto/fl_lossy/v' + image.version + '/' + image.public_id + '.' + image.format;
+  }
+  else{
+    return false;
+  }
+});
+
+
 
 /**
  * Registration
--- models/Promo.js
+++ models/Promo.js
@@ -14,19 +14,58 @@ var Promo = new keystone.List('Promo',{
   	label: 'Promoción',	
 });
 
+/**
+ * Local storage for promos
+ */
+var storage = new keystone.Storage({
+  adapter: keystone.Storage.Adapters.FS,
+  fs: {
+    path: 'public/files',
+    publicPath: 'files',
+  }
+});
+
 Promo.add('Promo',{
   title: { type: String, initial:true , required: true, label:'Título' },
   sucursal: {type: Types.Relationship, ref: 'Sucursal', initial: true, many: true, label: 'Sucursales donde aplica'},
   description: {type: Types.Html, wysiwyg: true, initial: true, required: true, label: 'Descripción'},
-  imagen: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen' },
+  image: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen escritorio' },
+  image_mobile: { type: Types.CloudinaryImage, initial:true ,publicID: 'slug', label:'Imagen teléfono' },
+  file: {type: Types.File, storage:storage, required: false, initial: false ,label: 'PDF de descarga'},
   terms_and_conditions: {type: Types.Html, wysiwyg: true, initial: true, required: true, label: 'Términos y condiciones'},
   init_date: {type: Date, initial: true, required: true, label: 'Fecha de inicio'},
   end_date: {type: Date, initial: true, required: true, label: 'Fecha de fin'}
 });
 
+/* acomplar url de cloudinary para traer imagen ligera */ 
+Slide.schema.virtual('image_mobile.cloudinaryURL').get(function () {
+  if ( this.image_mobile ) {
+    image = this.image_mobile;
+    return 'https://res.cloudinary.com/deytb0jub/image/upload/f_auto/q_auto/fl_lossy/v' + image.version + '/' + image.public_id + '.' + image.format;
+  }
+  else{
+    return false;
+  }
+});
+
+Slide.schema.virtual('image.cloudinaryURL').get(function () {
+  if ( this.image ) {
+    image = this.image;
+    return 'https://res.cloudinary.com/deytb0jub/image/upload/f_auto/q_auto/fl_lossy/v' + image.version + '/' + image.public_id + '.' + image.format;
+  }
+  else{
+    return false;
+  }
+});
+
+
+
+
 /**
  * Registration
  */
+
+
 
 
 Promo.defaultColumns = 'title, sucursal';
